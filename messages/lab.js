96e08bd69b1ab7b65786e57c91b4d4897852cd48
Zach Kirsch <zachkirsch@gmail.com>
2015-03-05 19:15:29 -0500

    JS creates request to JSON

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+function parse() {
+    // Step 1: create instance of object
+    request = new XMLHttpRequest();
+    
+    // Step 2: create or open HTTP request
+    request.open("GET", "data.json", true);
+    
+    // Step 3: Set up way to manage response --to a function
+    request.onreadystatechange = parseData;
+    
+    // Step 4: Execute the request
+    request.send();
+}
+
+function parseData() {
+    messages = JSON.parse(request.responseText);
+    console.log(messages);
+}
